2c35334651a26d278c1ac43a50e31178247e00aa
SrushtiPai05 <86263051+SrushtiPai05@users.noreply.github.com>
2021-08-07 21:01:24 +0530

    Add files via upload

---
+++ size of datatype.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"Size of char: 1 byte\n"<<"Size of int: 4 bytes\n"<<"Size of float: 4 bytes\n"<<"Size of double: 8 bytes\n";
+	return 0;
+}
