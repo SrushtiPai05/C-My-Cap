4c7d0706d6cab85c53e1d097fa1fd0ee7898fc71
SrushtiPai05 <86263051+SrushtiPai05@users.noreply.github.com>
2021-08-07 21:01:49 +0530

    Add files via upload

---
+++ product of two numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float num1, num2, product;
+	cout<<"Enter two numbers:\n";
+	cin>>num1>>num2;
+	
+	product= num1*num2;
+	cout<<"The product of two numbers is:"<<product;
+	
+}
